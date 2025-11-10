edb9454dc3dc0a3bec9ab39178acddbf7c070194
AkashRanjan1803 <akashranjan180307@gmail.com>
2025-11-10 17:44:28 +0530

    Create day 24 Q47.c

---
+++ day 24 Q47.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int n = 5; // number of rows
+
+    for (i = 1; i <= n; i++) {        
+        for (j = 1; j <= i; j++) {   
+            printf("*");
+        }
+        printf("\n"); // move to next line after each row
+    }
+
+    return 0;
+}
