1762458ab001d286c92f2875ec9d36aa8f2445d6
AkashRanjan1803 <akashranjan180307@gmail.com>
2025-11-10 17:34:47 +0530

    Create day 20 Q39.c

---
+++ day 20 Q39.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int num, dg, pro = 1;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num == 0) 
+    {
+        printf("Product of odd digits = 0\n");
+        return 0;
+    }
+    while (num > 0) {
+        dg = num % 10; 
+        if (digit % 2 != 0) 
+        {  
+            pro = pro*dg;
+        }
+        num = num / 10; 
+    }
+        printf("No odd digits found.\n");
+    return 0;
+}
+
+
+
+
