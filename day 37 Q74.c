957a58570c96145ac0efeceef2f4d15723f580b0
AkashRanjan1803 <akashranjan180307@gmail.com>
2025-11-23 19:18:17 +0530

    Create day 37 Q74.c

---
+++ day 37 Q74.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    int a[r][c], t[c][r];
+
+    printf("Enter elements of the matrix:\n");
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            t[j][i] = a[i][j];
+        }
+    }
+
+    printf("\nOriginal Matrix:\n");
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nTranspose Matrix:\n");
+    for(int i = 0; i < c; i++) {
+        for(int j = 0; j < r; j++) {
+            printf("%d ", t[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
