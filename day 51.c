70c0253ef4526327bb2c2ce14e3be545df635f41
AkashRanjan1803 <akashranjan180307@gmail.com>
2025-11-23 19:50:19 +0530

    Create day 51.c

---
+++ day 51.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int n, target;
+    
+    printf("Enter number of elements in sorted array: ");
+    scanf("%d", &n);
+    
+    int nums[n];
+    printf("Enter %d elements in sorted order: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    printf("Enter target element: ");
+    scanf("%d", &target);
+
+    int first = -1, last = -1;
+
+    
+    for (int i = 0; i < n; i++) {
+        if (nums[i] == target) {
+            first = i;
+            break;
+        }
+    }
+
+    
+    for (int i = n - 1; i >= 0; i--) {
+        if (nums[i] == target) {
+            last = i;
+            break;
+        }
+    }
+
+    printf("First occurrence index: %d\n", first);
+    printf("Last occurrence index: %d\n", last);
+
+    return 0;
+}
