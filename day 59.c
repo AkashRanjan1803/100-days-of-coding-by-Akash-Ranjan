9f5d97c605b3e16b6a96984d79e731aa65394db9
AkashRanjan1803 <akashranjan180307@gmail.com>
2025-12-01 19:35:43 +0530

    Create day 59.c

---
+++ day 59.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+
+   
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter array elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+   
+    printf("Enter value of k: ");
+    scanf("%d", &k);
+
+    if (k > n || k <= 0) {
+        printf("Invalid value of k\n");
+        return 0;
+    }
+
+   
+    int window_sum = 0;
+    for (int i = 0; i < k; i++) {
+        window_sum += arr[i];
+    }
+
+    int max_sum = window_sum;
+
+   
+    for (int i = k; i < n; i++) {
+        window_sum += arr[i] - arr[i - k];
+        if (window_sum > max_sum)
+            max_sum = window_sum;
+    }
+
+    printf("Maximum sum of subarrays of size %d is: %d\n", k, max_sum);
+
+    return 0;
+}
