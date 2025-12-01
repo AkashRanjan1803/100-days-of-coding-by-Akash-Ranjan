6d66bf6e4f25ef1e10f6056ca0a3bd4812fb3f77
AkashRanjan1803 <akashranjan180307@gmail.com>
2025-12-01 20:25:18 +0530

    Create day 91.c

---
+++ day 91.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+
+struct Student {
+    char name[50];
+    int roll_no;
+    float marks;
+};
+
+int main() {
+    struct Student s;
+
+    
+    printf("Enter student's name: ");
+    fgets(s.name, sizeof(s.name), stdin);
+    
+    if (s.name[strlen(s.name) - 1] == '\n') {
+        s.name[strlen(s.name) - 1] = '\0';
+    }
+
+    printf("Enter roll number: ");
+    scanf("%d", &s.roll_no);
+
+    printf("Enter marks: ");
+    scanf("%f", &s.marks);
+
+    
+    printf("\nStudent Details:\n");
+    printf("Name     : %s\n", s.name);
+    printf("Roll No  : %d\n", s.roll_no);
+    printf("Marks    : %.2f\n", s.marks);
+
+    return 0;
+}
