c35cb3958b77b4707342d7ca057a68994dcdc9c3
AkashRanjan1803 <akashranjan180307@gmail.com>
2025-12-01 19:41:02 +0530

    Create day 63.c

---
+++ day 63.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int main() {
+    int n, k;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter array elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter value of k: ");
+    scanf("%d", &k);
+
+    if (k <= 0 || k > n) {
+        printf("Invalid value of k\n");
+        return 0;
+    }
+
+   
+    qsort(arr, n, sizeof(int), compare);
+
+   
+    printf("The %dth smallest element is: %d\n", k, arr[k - 1]);
+
+    return 0;
+}
